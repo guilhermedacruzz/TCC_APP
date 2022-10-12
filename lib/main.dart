037f27b7388a4b5fcc52dc5cc7a6886929d00c35
indexGuilherme <guidacruz2004@gmail.com>
2022-10-12 13:12:59 -0300

    limpando tudo

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tcc/pages/carousel/page_carousel.dart';
-import 'package:tcc/pages/page_splash.dart';
-import 'notifiers/dark_theme_provider.dart';
-import 'pages/page_config.dart';
-import 'pages/page_home.dart';
-import 'pages/page_register_new_IOT.dart';
-import 'pages/page_sign_in.dart';
-import 'pages/page_sign_up.dart';
-import 'services/service_autentication.dart';
-import 'utils/style.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
-
-  @override
-  void initState() {
-    super.initState();
-    getCurrentAppTheme();
-  }
-
-  void getCurrentAppTheme() async {
-    themeChangeProvider.darkTheme =
-        await themeChangeProvider.darkThemePreference.getTheme();
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) {
-            return themeChangeProvider;
-          },
-        ),
-        ChangeNotifierProvider(
-          create: (_) => ServiceAutentication(),
-        ),
-      ],
-      child: Consumer<DarkThemeProvider>(
-        builder: (BuildContext contex, value, Widget? child) {
-          return MaterialApp(
-            title: "SAPA",
-            debugShowCheckedModeBanner: false,
-            initialRoute: PageCarousel.routeName,
-            theme: Styles.themeData(themeChangeProvider.darkTheme, context),
-            routes: {
-              PageRegisterNewIOT.routeName: (context) =>
-                  const PageRegisterNewIOT(),
-              PageConfig.routeName: (context) => const PageConfig(),
-              PageHome.routeName: (context) => const PageHome(),
-              PageSignIn.routeName: (context) => const PageSignIn(),
-              PageSignUp.routeName: (context) => const PageSignUp(),
-              PageSplash.routeName: (context) => const PageSplash(),
-              PageCarousel.routeName: (context) => const PageCarousel(),
-            },
-          );
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
       ),
+      home: Container()
     );
   }
 }
--- lib/utils/enums.dart
+++
@@ -1,5 +0,0 @@
-enum Status { working, idle, done }
-
-enum ActionResult { success, error, none }
-
-enum DataStatus { empty, loaded }
