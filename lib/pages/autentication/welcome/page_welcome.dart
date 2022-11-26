96e86fb65182f80ae12343322e5f1dc497851860
indexGuilherme <guidacruz2004@gmail.com>
2022-11-26 10:36:40 -0300

    mudando o welcome

--- lib/pages/autentication/welcome/page_welcome.dart
+++ lib/pages/autentication/welcome/page_welcome.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tcc/pages/autentication/welcome/page_welcome_backgrounsd_images.dart';
 import 'package:tcc/pages/autentication/welcome/page_welcome_data.dart';
 import 'package:tcc/pages/page_splash.dart';
 import 'package:tcc/widgets/forms/custom_button.dart';
@@ -25,82 +24,104 @@ class _PageWelcomeState extends State<PageWelcome> {
           return Container(
             width: double.maxFinite,
             height: double.maxFinite,
-            padding: const EdgeInsets.only(top: 120, right: 20, left: 20),
-            child: PageWelcomeBackgroundImage(
-              src: pageWelcomeData[index]["src"],
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        pageWelcomeData[index]["title"],
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Text(
-                        pageWelcomeData[index]["subtitle"],
-                        style: Theme.of(context).textTheme.subtitle1,
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: 250,
-                        child: Text(
-                          pageWelcomeData[index]["text"],
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                      ),
-                      const Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 40),
-                        child: (index == pageWelcomeData.length - 1)
-                            ? SizedBox(
-                                width: 120,
-                                height: 45,
-                                child: CustomButton(
-                                  widget: Text(
-                                    "Começar",
-                                    style: Theme.of(context).textTheme.button,
+            padding: const EdgeInsets.only(top: 80, right: 35, left: 35),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          pageWelcomeData[index]["title"],
+                          style:
+                              Theme.of(context).textTheme.headline4!.copyWith(
+                                    color: Theme.of(context).primaryColorDark,
                                   ),
-                                  onAction: () {
-                                    Navigator.of(context)
-                                        .pushNamed(PageSplash.routeName);
-                                  },
-                                ),
-                              )
-                            : Icon(
-                                Icons.arrow_downward,
-                                size: 35,
-                                color: Theme.of(context).indicatorColor,
+                        ),
+                        Text(
+                          pageWelcomeData[index]["subtitle"],
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: 250,
+                          child: Text(
+                            pageWelcomeData[index]["text"],
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        ...List.generate(
+                          pageWelcomeData.length,
+                          (indexDots) {
+                            return Container(
+                              margin: const EdgeInsets.only(bottom: 2),
+                              width: 8,
+                              height: (indexDots == index) ? 25 : 8,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: (indexDots == index)
+                                    ? Theme.of(context).indicatorColor
+                                    : Theme.of(context)
+                                        .indicatorColor
+                                        .withOpacity(0.5),
                               ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      ...List.generate(
-                        pageWelcomeData.length,
-                        (indexDots) {
-                          return Container(
-                            margin: const EdgeInsets.only(bottom: 2),
-                            width: 8,
-                            height: (indexDots == index) ? 25 : 8,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: (indexDots == index)
-                                  ? Theme.of(context).indicatorColor
-                                  : Theme.of(context)
-                                      .indicatorColor
-                                      .withOpacity(0.5),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      pageWelcomeData[index]["src"],
+                      fit: BoxFit.cover,
+                      width: 300,
+                      height: 300,
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: (index == pageWelcomeData.length - 1)
+                      ? SizedBox(
+                          width: 120,
+                          height: 45,
+                          child: CustomButton(
+                            widget: Text(
+                              "Começar",
+                              style: Theme.of(context).textTheme.button,
                             ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                            onAction: () {
+                              Navigator.of(context)
+                                  .pushNamed(PageSplash.routeName);
+                            },
+                          ),
+                        )
+                      : Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColorDark,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Icon(
+                            Icons.arrow_downward,
+                            size: 35,
+                            color: Theme.of(context).backgroundColor,
+                          ),
+                        ),
+                ),
+              ],
             ),
           );
         },
--- lib/pages/autentication/welcome/page_welcome_data.dart
+++ lib/pages/autentication/welcome/page_welcome_data.dart
@@ -1,19 +1,19 @@
 List pageWelcomeData = [
   {
     "title": "Aumente a sua proteção",
-    "subtitle": "by Cruz",
+    "subtitle": "por Guilherme da Cruz",
     "text": "Com o nosso sistema, sua residência se tornará muito mais segura.",
     "src":"assets/images/welcome/welcome_image_1.png",
   },
   {
     "title": "Totalmente Personalizável",
-    "subtitle": "by Cruz",
+    "subtitle": "por Guilherme da Cruz",
     "text": "Você pode escolher as configurações do seu dispositivo IOT.",
     "src":"assets/images/welcome/welcome_image_2.png",
   },
   {
     "title": "Vamos Lá",
-    "subtitle": "by Cruz",
+    "subtitle": "por Guilherme da Cruz",
     "text": "Vamos lá, o seu aplicativo já está pronto para o uso.",
     "src":"assets/images/welcome/welcome_image_3.png",
   },
